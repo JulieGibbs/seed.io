3529e482e884bd59fb6aed75a5f7c72c3c5450f7
JulieGibbs <harrisonbeckham1106@gmail.com>
2022-12-07 16:51:08 +0100

    add files for bottom navigation

--- app/src/main/java/com/example/seed/MainActivity.kt
+++ app/src/main/java/com/example/seed/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.seed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.seed.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
