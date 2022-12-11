af53823c521d4610365ee18bcbdbed41a8110324
JulieGibbs <harrisonbeckham1106@gmail.com>
2022-12-11 15:26:40 +0100

    add EditProfile capabilities

--- app/src/main/java/com/example/seed/fragments/EditProfileFragment.kt
+++ app/src/main/java/com/example/seed/fragments/EditProfileFragment.kt
@@ -5,56 +5,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.seed.R
+import androidx.navigation.fragment.findNavController
+import com.example.seed.data.User
+import com.example.seed.databinding.FragmentEditProfileBinding
+import com.example.seed.viewmodel.UserViewModel
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TimelineFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class EditProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentEditProfileBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var userId : String = ProfileFragment.NOT_LOGGED_IN_USER_ID
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timeline, container, false)
+    ): View {
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        if (firebaseAuth.currentUser != null) {
+            userId = firebaseAuth.currentUser!!.uid
+        }
+
+        // prepopulate the fields
+        UserViewModel.getUserInfo(
+            userId = userId,
+            handleUserFound = ::prepopulateFields
+        )
+
+        binding.btnEditProfile.setOnClickListener {
+            UserViewModel.updateUser(
+                userId = userId,
+                newUser = User(
+                    username = binding.etUsername.text.toString(),
+                    uid = userId,
+                    bio = binding.etBio.text.toString(),
+                    imgURL = binding.etImageUrl.text.toString()
+                ),
+                handleSuccess = ::navigateToProfileFragment
+            )
+        }
+
+        binding.btnCancel.setOnClickListener {
+            navigateToProfileFragment()
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TimelineFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TimelineFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun prepopulateFields(user: User){
+        binding.etUsername.setText(user.username)
+        binding.etBio.setText(user.bio)
+        binding.etImageUrl.setText(user.imgURL)
+    }
+
+    private fun navigateToProfileFragment(){
+        val action = EditProfileFragmentDirections.actionEditProfileFragmentToProfileFragment()
+        findNavController().navigate(action)
     }
 }
